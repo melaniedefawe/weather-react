072e430431377dd74049ebb476b278e214fea9ff
valhalyria <melanie.defawe@gmail.com>
2022-07-08 13:30:30 +0200

    Integrated OpenWeather API

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,52 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import axios from "axios";
 import Search from "./Search";
 import CityDate from "./CityDate";
 import WeatherDetail from "./WeatherDetail";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function WeatherApp() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="box">
-          <Search />
-          <div className="row">
-            <div className="col-7">
-              <CityDate city="Berlin" day="Wednesday" hour="11:10" />
-              <br />
-              <WeatherDetail humidity={10} wind={6} rain={0} />
-            </div>
-            <div class="col-4 small-box">
-              <div className="current">
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                  alt=""
-                  id="icon"
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      felt: response.data.main.feels_like,
+      icon: response.data.weather[0].icon,
+      ready: true,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="box">
+            <Search />
+            <div className="row">
+              <div className="col-7">
+                <CityDate
+                  city={weatherData.city}
+                  day="Wednesday"
+                  hour="11:10"
                 />
                 <br />
-                <h1 className="temperature">8 </h1>
-                <a href="/" className="celsius active" rel="noreferrer">
-                  °C
-                </a>
-                <span className="separator"> | </span>
-                <a href="/" className="fahrenheit" rel="noreferrer">
-                  °F
-                </a>
-                <p className="description">Clear</p>
-                <p className="felt-temperature">Feels like 11°C</p>
+                <WeatherDetail
+                  humidity={weatherData.humidity}
+                  wind={weatherData.wind}
+                  rain={0}
+                />
+              </div>
+              <div class="col-4 small-box">
+                <div className="current">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+                    alt={weatherData.description}
+                  />
+                  <br />
+                  <h1 className="temperature">
+                    {Math.round(weatherData.temperature)}{" "}
+                  </h1>
+                  <a href="/" className="celsius active" rel="noreferrer">
+                    °C
+                  </a>
+                  <span className="separator"> | </span>
+                  <a href="/" className="fahrenheit" rel="noreferrer">
+                    °F
+                  </a>
+                  <p className="description">{weatherData.description}</p>
+                  <p className="felt-temperature">
+                    Feels like {Math.round(weatherData.felt)}°C
+                  </p>
+                </div>
               </div>
             </div>
           </div>
+          <a
+            href="https://github.com/valhalyria/weather-react"
+            target="blank"
+            rel="noreferrer"
+          >
+            Open-source code by Melanie Defawe
+          </a>
         </div>
-        <a
-          href="https://github.com/valhalyria/weather-react"
-          target="blank"
-          rel="noreferrer"
-        >
-          Open-source code by Melanie Defawe
-        </a>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "7fbc99e26b128af1fc9815e393cfbb4b";
+    let city = "Berlin";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
